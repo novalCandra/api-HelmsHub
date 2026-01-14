84b3edaf01934531e2a2ef5313e555ffb96988d2
novalCandra <dinatanovel5@gmail.com>
2026-01-14 22:09:13 +0700

    feat : ✅ Borrowed Done

--- app/Http/Controllers/BorrowedController.php
+++ app/Http/Controllers/BorrowedController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Borrowed;
+use App\Models\Helm;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BorrowedController extends Controller
 {
@@ -12,7 +14,13 @@ class BorrowedController extends Controller
      */
     public function index()
     {
-        //
+        $BorrowedAll = Borrowed::with(['Users', 'Helm'])->get();
+
+        return response()->json([
+            "status" => true,
+            "message" => "Success add Borrowed All",
+            "data" => $BorrowedAll
+        ],  200);
     }
 
     /**
@@ -28,15 +36,41 @@ class BorrowedController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            "helmet_id" => "required|exists:helms,id",
+            "borrow_date" => "required|date",
+            "due_date" => "required|date",
+            "return_date" => "required|date",
+        ]);
+
+        $user = Auth::user();
+
+        $BorrowCreated = Borrowed::create([
+            "user_id" => $user->id,
+            "helmet_id" => $request->helmet_id,
+            "borrow_date" => $request->borrow_date,
+            "due_date" => $request->due_date,
+            "return_date" => $request->return_date
+        ]);
+
+        return response()->json([
+            "status" => true,
+            "message"  => "Success add Browwed",
+            "data" => $BorrowCreated
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Borrowed $borrowed)
+    public function show($id)
     {
-        //
+        $borrowedDetails = Borrowed::with(['Users', 'Helm'])->findOrFail($id);
+        return response()->json([
+            "status" => true,
+            "message" => "success details Borrowed",
+            "data" => $borrowedDetails
+        ], 200);
     }
 
     /**
--- app/Models/Borrowed.php
+++ app/Models/Borrowed.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Borrowed extends Model
 {
     protected $guarded = [];
+
+    public function Users()
+    {
+        return parent::belongsTo(User::class, "user_id");
+    }
+
+    public function Helm()
+    {
+        return parent::belongsTo(Helm::class, "helmet_id");
+    }
 }
