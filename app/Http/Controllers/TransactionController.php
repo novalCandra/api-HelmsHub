b04a210c086b175a0e6878b0ed4bf65740395327
novalCandra <dinatanovel5@gmail.com>
2026-01-15 13:02:08 +0700

    feat : ✅ done Transactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
@@ -12,31 +13,65 @@ class TransactionController extends Controller
      */
     public function index()
     {
-        //
+        $transactionAll = Transaction::paginate(10);
+        return response()->json([
+            "status" => true,
+            "message" => "All Transaction",
+            "data" => $transactionAll
+        ]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+    public function create() {}
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            "borrowed_id" => "required|exists:borroweds,id",
+            "helm_id" => "required|exists:helms,id",
+            "total_price" => "required|numeric",
+            "total_price" => "required|numeric",
+            "fine_amount" => "required|numeric",
+            "payment_method" => "required|string",
+            "payment_status" => "required|string",
+            "payment_date" => "required|date"
+        ]);
+
+        $user = Auth::user();
+        $CreateTransaksi = Transaction::create([
+            "borrowed_id" => $request->borrowed_id,
+            "helm_id" => $request->helm_id,
+            "user_id" => $user->id,
+            "total_price" => $request->total_price,
+            "fine_amount" => $request->fine_amount,
+            "payment_method" => $request->payment_method,
+            "payment_status" => $request->payment_status,
+            "payment_date" => $request->payment_date
+        ]);
+
+        return response()->json([
+            "status" => true,
+            "message" => "success transaksi users",
+            "data" => $CreateTransaksi
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Transaction $transaction)
+    public function show($id)
     {
-        //
+        $TransactionId = Transaction::findOrFail($id);
+        return response()->json([
+            "status" => true,
+            "message" => "details Transaction",
+            "data" => $TransactionId
+        ], 201);
     }
 
     /**
