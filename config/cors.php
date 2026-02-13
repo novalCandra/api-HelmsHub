159381a02911bab89d063b91af8bcda0248ab78c
novalCandra <dinatanovel5@gmail.com>
2026-02-13 09:37:22 +0700

    feat :  ✅ Done CORS

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'], 
+    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
